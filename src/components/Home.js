7fe6ed798bccf6a341bac1257412c5128333e721
soo.yongjie <soo.yongjie@growthops.asia>
2022-07-04 16:13:54 +0800

    First commit

---
+++ src/components/Home.js
@@ -0,0 +1,39 @@
+import Villagers from './Villagers';
+import { useState } from "react"
+
+
+function Home() {
+    const [query, setQuery] = useState("")
+
+    const capitalizeFirstLetter = (string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    const updateVillagers = (event) => {
+        setQuery(capitalizeFirstLetter(event.target.value))
+    }
+
+
+    return (
+        <div className="container">
+            <div className="welcome-container">
+                <div className="box">
+                    <div className="box-header">
+                        Welcome
+                    </div>
+                    <div className="box-body">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit soluta possimus similique quos aut laborum tempore delectus unde, quaerat accusamus provident voluptas nobis minima magnam ab natus animi et. Nobis eos dicta corporis consequatur, alias saepe asperiores optio iusto reprehenderit?
+                    </div>
+                </div>
+            </div>
+            <div className="search-container">
+                <input type="text" name="acnh-search" onInput={updateVillagers} />
+                <div className="dash"></div>
+            </div>
+            <Villagers query={query} />
+        </div>
+    )
+}
+
+
+export default Home
---
+++ src/components/Villagers.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import VillagerList from '../villagers.json';
+import { useState } from "react"
+import img from '../image/villagers_poster/NpcNmlSqu07.png'
+
+
+function Villagers({ query }) {
+    const [villagers, setVillagers] = useState(VillagerList)
+
+    let species = Object.values(villagers).filter(vil => vil.species.includes(query))
+    let name = Object.values(villagers).filter(vil => vil.name.nameUSen.includes(query))
+    let personality = Object.values(villagers).filter(vil => vil.personality.includes(query))
+
+    if (Object.keys(species).length == 0 && Object.keys(name).length == 0 && Object.keys(personality).length == 0) console.log("WHAT");
+
+    if (Object.keys(name).length != 0)
+        return (
+            <div className='villagers'>
+                <div className='villager-container'>
+                    {
+                        name.map((villager) => (
+                            <div className="villager">
+                                <div className="title" style={{ backgroundColor: villager.bubble_color, color: villager.text_color }} >
+                                    {villager.name.nameEUen}
+                                </div>
+                                <img src={require(`../image/villagers_poster/NpcNml${villager.file_name}.png`)} alt="" />
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+        )
+    if (Object.keys(species).length != 0)
+        return (
+            <div className='villagers'>
+                <div className='villager-container'>
+                    {
+                        species.map((villager) => (
+                            <div className="villager">
+                                <div className="title" style={{ backgroundColor: villager.bubble_color, color: villager.text_color }} >
+                                    {villager.name.nameEUen}
+                                </div>
+                                <img src={require(`../image/villagers_poster/NpcNml${villager.file_name}.png`)} alt="" />
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+        )
+
+    if (Object.keys(personality).length != 0)
+        return (
+            <div className='villagers'>
+                <div className='villager-container'>
+                    {
+                        personality.map((villager) => (
+                            <div className="villager">
+                                <div className="title" style={{ backgroundColor: villager.bubble_color, color: villager.text_color }} >
+                                    {villager.name.nameEUen}
+                                </div>
+                                <img src={require(`../image/villagers_poster/NpcNml${villager.file_name}.png`)} alt="" />
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+        )
+}
+
+export default Villagers
